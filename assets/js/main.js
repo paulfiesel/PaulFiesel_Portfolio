e88083060bbb77789383aabf8932a495ae42bc26
Paul Fiesel <paul.fiesel@gmail.com>
2024-02-10 22:59:11 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,12 @@ if (navClose) {
 }
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== DARK LIGHT THEME ===============*/
