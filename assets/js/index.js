25072a4a56cf42eda34f5ef6e288af323047d5d0
Paul Fiesel <paul.fiesel@gmail.com>
2024-02-17 22:09:52 -0600

    Added swiper.js functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,6 +22,17 @@ modalClose.forEach((modalClose) => {
 })
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+const swiperTestimonial = new Swiper('.testimonial__swiper', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
 
+
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+    },
+});
 
 /*=============== SHOW SCROLL UP ===============*/
