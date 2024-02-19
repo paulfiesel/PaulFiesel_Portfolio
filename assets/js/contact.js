95e3c9fff7017c70c03b5efec48ded6e3237251d
Paul Fiesel <paul.fiesel@gmail.com>
2024-02-18 23:58:03 -0600

    Edited contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,11 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+}
+
+// serviceID - templateID - #form - publickKey
+
+contactForm.addEventListener('submit', sendEmail)
